c7e0e6a06ba4ac4730b236616973ba58c63121a2
jan.sieber <jan.sieber@23581414-1c31-4b1c-9b34-2124a82cf4e4>
2014-04-13 21:40:05 +0000

    A    dde_biftool/tools AM   dde_biftool/tools/release.sh AM   dde_biftool/tools/c_insert.py
    Two scripts for creation of distributable versions:
    
    release.sh: bash file doing svn export, compiling manual and cleaning
    up temporary files and tools
    
    c_insert.py: python script replacing (c) line with appropriate version
    taken from Id line, and removing Id line in all files where it occurs

---
+++ trunk/dde_biftool/tools/release.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#
+# $Id: release.sh 39 2013-06-13 12:31:32Z jansie $
+#
+set -e 
+version=$1
+name="ddebiftool_v"$version
+destdir="tags/$name"
+zip=$name".zip"
+files="*/*.*"
+rm -rf $destdir
+cp -urp ~/tmp/leuven-export/dde_biftool $destdir
+#svn export ^/trunk  $destdir --native-eol CRLF
+base=`pwd`
+destdir=$base/$destdir
+#
+# compile manual
+cd $destdir/manual
+tex="TWcover manual ExtensionChanges-v3"
+echo '\newcommand{\version}{'$version'}' >version.tex
+for x in $tex; do
+    pdflatex $x && pdflatex $x && pdflatex $x && \
+    bibtex $x && pdflatex $x && pdflatex $x
+done
+pdftk A=TWcover.pdf B=manual.pdf cat A B output all.pdf
+mv all.pdf $destdir/manual.pdf
+mv ExtensionChanges-v3.pdf $destdir
+# clean up manual creation
+cd $destdir
+#
+#
+python $destdir/tools/c_insert.py $destdir $version
+
+# remove folders not intended for distibution
+rm -rf  tools manual
+
+#correct author name
+#find . -type f -print0 | xargs -0 sed -i '/$Id/s/jansie/Jan Sieber/g'
+#find . -type f -print0 | xargs -0 sed -i '/$Id/s/js543/Jan Sieber/g'
+
+#insert release number
+#find . -type f -print0 | xargs -0 sed -i '/$Id/s/$Id/$Id('"$version"')/g'
+
+# zip
+cd ..
+rm -rf $zip
+zip -r $zip $name
+# copy zip file to Dropbox
+#cp -p $zip $HOME/Dropbox/Public/ddebiftool_ext.zip
+
