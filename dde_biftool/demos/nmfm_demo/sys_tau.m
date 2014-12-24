164c0d223a1b2a71ce6649416c969dd942988248
jan.sieber <j.sieber@exeter.ac.uk>
2014-12-24 13:19:50 +0000

    Added files for introductory normalform demo:
    A    nmfm_demo
    AM   nmfm_demo/sys_mfderi.m
    AM   nmfm_demo/sys_tau.m
    A    nmfm_demo/html
    AM   nmfm_demo/html/nmfm_demo_eq27608.png
    AM   nmfm_demo/html/nmfm_demo_eq23839.png
    AM   nmfm_demo/html/nmfm_demo_eq48238.png
    AM   nmfm_demo/html/nmfm_demo_eq36368.png
    AM   nmfm_demo/html/nmfm_demo_eq27773.png
    AM   nmfm_demo/html/nmfm_demo_01.png
    AM   nmfm_demo/html/nmfm_demo_eq53893.png
    AM   nmfm_demo/html/nmfm_demo_eq76729.png
    AM   nmfm_demo/html/nmfm_demo_eq75785.png
    AM   nmfm_demo/html/nmfm_demo_eq80350.png
    AM   nmfm_demo/html/nmfm_demo_eq50353.png
    AM   nmfm_demo/html/nmfm_demo_eq07064.png
    AM   nmfm_demo/html/nmfm_demo_eq28601.png
    AM   nmfm_demo/html/nmfm_demo_eq01297.png
    AM   nmfm_demo/html/nmfm_demo_eq61256.png
    AM   nmfm_demo/html/nmfm_demo_eq05186.png
    AM   nmfm_demo/sys_rhs.m
    AM   nmfm_demo/nmfm_demo.m
    AM   nmfm_demo/sys_deri.m

---
+++ dde_biftool/demos/nmfm_demo/sys_tau.m
@@ -0,0 +1,11 @@
+function tau = sys_tau()
+%% Delay indices
+% par: a b c d tau1 tau2
+% xx:   x1(t), x1(t-tau1), x1(t-tau2)
+%       x2(t), x2(t-tau1), x2(t-tau2)
+%
+% $Id$
+%
+%%
+tau = [5 6];
+end
