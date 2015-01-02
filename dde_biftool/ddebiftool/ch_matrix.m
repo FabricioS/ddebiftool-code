1ee69fab1152d2bf542c81cf6468d134dfa8bc89
jan.sieber <j.sieber@exeter.ac.uk>
2015-01-02 21:49:37 +0000

    Updated ch_matrix to be able to cope with either a nx1 or a nx(ntau+1) input xx:
    M    dde_biftool/ddebiftool/ch_matrix.m
    
    Updated manual to include normalforms:
    
    M    dde_biftool/manual/manual.bib
    M    dde_biftool/manual/manual.tex

--- dde_biftool/ddebiftool/ch_matrix.m
+++ dde_biftool/ddebiftool/ch_matrix.m
@@ -3,14 +3,17 @@ function Delta=ch_matrix(funcs,xx,par,lambda,varargin)
 % function Delta=ch_matrx(funcs,x,par,l)
 % INPUT:
 %   funcs problem functions
-%	x steady state solution in R^n
+%	x steady state solution in R^n (either n x (ntau+1), or n x 1)
 %	par parameter values
 %	l root in C
 %   optional named argument: 'deri', integer (default 0) return derivative
 %   of characteristic matrix wrt lambda
 % OUTPUT: 
 %	D characteristic matrix in C^(n x n)
-
+%
+% |xx| is assumed to be equilibrium such that |xx(:,2:end)| are ignored and
+% |xx(:,ones(1,(ntau+1))| is used instead.
+%
 % (c) DDE-BIFTOOL v. 2.00, 23/11/2001
 %
 % $Id$
@@ -18,23 +21,26 @@ function Delta=ch_matrix(funcs,xx,par,lambda,varargin)
 %%
 default={'deri',0};
 options=dde_set_options(default,varargin);
-[n,r] = size(xx); % n = #coordinates, r = #delays+1
+n = size(xx,1); % n = #coordinates,
 if ~funcs.tp_del
-   taus = par(funcs.sys_tau());
-   taus = [0, taus]; % First delay zero
+    taus = par(funcs.sys_tau());
+    taus = [0, taus]; % First delay zero
+    r=length(taus)+1; % number of delays, r = #delays+1
 else % state-dependent delays
-   taus = zeros(1,r); % First delay zero
-   for i = 2:r
-      taus(i) = funcs.sys_tau(i-1,xx(:,1:i-1),par);
-   end
+    r=funcs.sys_ntau()+1;
+    taus = zeros(1,r); % First delay zero
+    for i = 2:r
+        taus(i) = funcs.sys_tau(i-1,xx(:,ones(1,i)),par);
+    end
 end
+xx=xx(:,ones(1,r));
 lfac=[lambda,1,zeros(1,options.deri-1)];
 tpow=options.deri;
 tfac=(-1)^options.deri;
 Delta = lfac(options.deri+1)*eye(n);
 for k = 1:r % For every delay
-   Ak = funcs.sys_deri(xx,par,k-1,[],[]);
-   Delta = Delta -tfac*taus(k)^tpow*Ak*exp(-lambda*taus(k));
+    Ak = funcs.sys_deri(xx,par,k-1,[],[]);
+    Delta = Delta -tfac*taus(k)^tpow*Ak*exp(-lambda*taus(k));
 end
 
 end
