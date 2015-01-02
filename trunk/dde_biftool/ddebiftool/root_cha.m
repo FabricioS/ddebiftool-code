d3589924a0b03aa3e7e2b2603a5c0d502814516a
jan.sieber <jan.sieber@23581414-1c31-4b1c-9b34-2124a82cf4e4>
2015-01-02 18:33:21 +0000

    Modify characteristic matrix function to return derivatives of abitrary order.
    M    root_cha.m

--- trunk/dde_biftool/ddebiftool/root_cha.m
+++ trunk/dde_biftool/ddebiftool/root_cha.m
@@ -1,47 +1,40 @@
-function [D,dD]=root_cha(funcs,x,par,l)
+function Delta=ch_matrix(funcs,xx,par,lambda,varargin)
 %% Characteristic matrix and its derivatives
-% function [D,dD]=ch_matrx(funcs,x,par,l)
+% function Delta=ch_matrx(funcs,x,par,l)
 % INPUT:
 %   funcs problem functions
 %	x steady state solution in R^n
 %	par parameter values
 %	l root in C
+%   optional named argument: 'deri', integer (default 0) return derivative
+%   of characteristic matrix wrt lambda
 % OUTPUT: 
 %	D characteristic matrix in C^(n x n)
-%	dD derivative of characteristic matrix wrt l in C^(n x n)
 
 % (c) DDE-BIFTOOL v. 2.00, 23/11/2001
 %
-% Id
+% $Id$
 %
 %%
-sys_tau=funcs.sys_tau;
-sys_ntau=funcs.sys_ntau;
-sys_deri=funcs.sys_deri;
-
-tp_del=funcs.tp_del;
-if tp_del==0
-  tau=[0 par(sys_tau())];
-  m=length(tau)-1;
-  xx=x(:,ones(m+1,1));
-else
-  m=sys_ntau();
-  xx=x(:,ones(m+1,1));
-  t_tau=NaN(1,m);
-  for j=1:m
-    t_tau(j)=sys_tau(j,xx,par);
-  end;
-  tau=[0 t_tau];
+default={'deri',0};
+options=dde_set_options(default,varargin);
+[n,r] = size(xx); % n = #coordinates, r = #delays+1
+if ~funcs.tp_del
+   taus = par(funcs.sys_tau());
+   taus = [0, taus]; % First delay zero
+else % state-dependent delays
+   taus = zeros(1,r); % First delay zero
+   for i = 2:r
+      taus(i) = funcs.sys_tau(i-1,xx(:,1:i-1),par);
+   end
+end
+lfac=[lambda,1,zeros(1,options.deri-1)];
+tpow=options.deri;
+tfac=(-1)^options.deri;
+Delta = lfac(options.deri+1)*eye(n);
+for k = 1:r % For every delay
+   Ak = funcs.sys_deri(xx,par,k-1,[],[]);
+   Delta = Delta -tfac*taus(k)^tpow*Ak*exp(-lambda*taus(k));
 end
 
-n=length(x);
-
-D=l*eye(n);
-dD=eye(n);
-
-for j=0:m
-  B=sys_deri(xx,par,j,[],[])*exp(-l*tau(j+1));
-  D=D-B;
-  dD=dD+tau(j+1)*B;
-end
 end
