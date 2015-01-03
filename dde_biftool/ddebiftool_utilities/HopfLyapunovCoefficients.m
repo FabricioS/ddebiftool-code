fad9cd3aff9642281914b7040e509d4fbb5a7780
jan.sieber <j.sieber@exeter.ac.uk>
2015-01-03 01:49:19 +0000

    Included comments and added Id line in
    M    ddebiftool_utilities/HopfLyapunovCoefficients.m

--- dde_biftool/ddebiftool_utilities/HopfLyapunovCoefficients.m
+++ dde_biftool/ddebiftool_utilities/HopfLyapunovCoefficients.m
@@ -1,5 +1,28 @@
 %% first Lyapunov coefficient L1 in Hopf point along stst branch or Hopf branch
 function [L1,L1low]=HopfLyapunovCoefficients(funcs,branch,varargin)
+%% Input
+%
+% * funcs: problem functions
+% * branch: hopf branch or array of points of kind 'hopf'
+% * optional name-value pairs: 
+% * 'use_nullvectors' (boolean, default false) use nullvectors from
+% previous point along branch to compute new nullvectors by bordering
+% (otherwise, svd is used)
+% * 'sys_mfderi': update sys_mfderi field of funcs (useful for
+% finite-difference approzimation only)
+%
+%% Output
+%
+% * L1: array of L1 coefficients along branch
+% * L1low: equals L1 if funcs.sys_mfderi_provided is true, otherwise (for
+% finite-difference approximation) a low-er-order estimate of L1
+% coefficients. Use L1-L1low to estimate the accuracy of L1. If this is
+% large, finite-differences are problematic. If this is small it may(!) be
+% ok.
+%
+% $Id$
+%
+%%
 default={'use_nullvectors',false,'sys_mfderi',{}};
 options=dde_set_options(default,varargin);
 if isfield(branch,'point')
