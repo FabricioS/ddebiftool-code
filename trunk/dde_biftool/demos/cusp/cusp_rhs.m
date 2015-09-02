a604ebba3122c8cfb46fc9547ddfe82927d39be6
jansieber <jansieber@23581414-1c31-4b1c-9b34-2124a82cf4e4>
2015-09-02 21:58:15 +0000

    Added demo cusp. It works with the current revision without any modification (using br_bifdet).
    A    dde_biftool/demos/cusp
    AM   dde_biftool/demos/cusp/cusp_demo.m
    
    Added the equations at the top and an Id line
    
    AM   dde_biftool/demos/cusp/cusp_deri.m
    AM   dde_biftool/demos/cusp/cusp_mfderi.m
    AM   dde_biftool/demos/cusp/cusp_rhs.m
    
    renamed the files sys_*.m to cusp_*.m to give them a more descriptive
    name.
    
    A    dde_biftool/demos/cusp/html
    AM   dde_biftool/demos/cusp/html/cusp_demo.html
    AM   dde_biftool/demos/cusp/html/cusp_demo.png
    AM   dde_biftool/demos/cusp/html/cusp_demo_01.png
    AM   dde_biftool/demos/cusp/html/cusp_demo_02.png
    AM   dde_biftool/demos/cusp/html/cusp_demo_03.png
    AM   dde_biftool/demos/cusp/html/cusp_demo_04.png
    AM   dde_biftool/demos/cusp/html/cusp_demo_05.png
    AM   dde_biftool/demos/cusp/html/cusp_demo_eq06076393689779727659.png
    AM   dde_biftool/demos/cusp/html/cusp_demo_eq16360302275264720844.png
    
    created "publish" from matlab (which works)
    The demo needs to be expanded.

---
+++ trunk/dde_biftool/demos/cusp/cusp_rhs.m
@@ -0,0 +1,12 @@
+function f = sys_rhs(xx,par)
+
+%par = [q11,q12,q21,e1,e2]
+
+% f(1,1) = -xx(1,1)+par(1)/(1+exp(-4*xx(1,2)))-par(2)*xx(2,2)+par(4);
+% f(2,1) = -xx(2,1)+par(3)/(1+exp(-4*xx(1,2)))+par(5);
+
+alpha = @(u) 1/(1+exp(-4*u))-1/2;
+
+f(1,1) = -xx(1,1)+par(1)*alpha(xx(1,2))-par(2)*xx(2,2)+par(4);
+f(2,1) = -xx(2,1)+par(3)*alpha(xx(1,2))+par(5);
+end
